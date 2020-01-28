59758e312d12317bb5f35fe12b81a47709b66454
Matthias Kutscher <matthias.kutscher@wiredminds.de>
2020-01-28 13:41:33 +0100

    - allow custom control "commands", not only single line "command" entries, also hide controls with a input

--- ui/control.go
+++ ui/control.go
@@ -67,7 +67,7 @@ func (m *controlPanel) getCustomControl() []*octoprint.ControlDefinition {
 
 	for _, c := range r.Controls {
 		for _, cc := range c.Children {
-			if ((cc.Command != "" || cc.Commands != "") && cc.Input == nil) {
+			if ((cc.Command != "" || cc.Commands != nil) && cc.Input == nil) {
 				control = append(control, cc)
 			}
 		}
